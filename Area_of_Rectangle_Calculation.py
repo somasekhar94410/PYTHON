629e1e0d20dd2dac3fa13f0c43b84884dd687ec5
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-16 09:45:48 +0530

    Create Area_of_Rectangle_Calculation.py
    This program calculates the area of a rectangle using the formula:
    
    A=l×b
    
    where:
    
    A = Area of the rectangle
    l = Length of the rectangle
    b = Breadth (Width) of the rectangle
    
    The program accepts the length and breadth from the user, computes the area, and displays the result.

---
+++ Area_of_Rectangle_Calculation.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+area = length * breadth
+
+print("Area of the rectangle =", area)
