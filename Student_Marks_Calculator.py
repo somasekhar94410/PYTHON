2da9ce36ce59298d4f2358896d3c726087862573
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-16 10:11:16 +0530

    Create Student_Marks_Calculator.py
    This program accepts marks obtained in five subjects from the user and calculates the total marks by adding all the subject marks. It demonstrates the use of variables, user input, and arithmetic operations in Python.

---
+++ Student_Marks_Calculator.py
@@ -0,0 +1,9 @@
+subject1 = float(input("Enter marks of Subject 1: "))
+subject2 = float(input("Enter marks of Subject 2: "))
+subject3 = float(input("Enter marks of Subject 3: "))
+subject4 = float(input("Enter marks of Subject 4: "))
+subject5 = float(input("Enter marks of Subject 5: "))
+
+total = subject1 + subject2 + subject3 + subject4 + subject5
+
+print("Total Marks Obtained =", total)
