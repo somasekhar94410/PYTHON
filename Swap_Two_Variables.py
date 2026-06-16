a95320edf1cd5aecacd02923ccd5e4a5e1056397
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-16 10:07:42 +0530

    Create Swap_Two_Variables.py
    This program swaps (interchanges) the values of two variables. It takes two values as input from the user, exchanges their values using Python's multiple assignment feature, and displays the result before and after swapping.

---
+++ Swap_Two_Variables.py
@@ -0,0 +1,12 @@
+a = input("Enter the first value: ")
+b = input("Enter the second value: ")
+
+print("\nBefore Swapping")
+print("a =", a)
+print("b =", b)
+
+a, b = b, a
+
+print("\nAfter Swapping")
+print("a =", a)
+print("b =", b)
