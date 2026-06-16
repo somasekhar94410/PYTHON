6967d262376d2c52adbb8baa9309f80483940a21
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-16 09:50:39 +0530

    Create  Unit_Conversion_Program.py
    This program performs basic unit conversions. It accepts values from the user and converts them into smaller units using standard conversion factors.
    
    Conversion Factors:
    
    1 Kilometer = 1000 Meters
    1 Meter = 100 Centimeters
    1 Kilogram = 1000 Grams

---
+++ Unit_Conversion_Program.py
@@ -0,0 +1,11 @@
+kilometers = float(input("Enter distance in kilometers: "))
+meters = float(input("Enter distance in meters: "))
+kilograms = float(input("Enter weight in kilograms: "))
+
+km_to_m = kilometers * 1000
+m_to_cm = meters * 100
+kg_to_g = kilograms * 1000
+
+print("Kilometers to Meters =", km_to_m)
+print("Meters to Centimeters =", m_to_cm)
+print("Kilograms to Grams =", kg_to_g)
