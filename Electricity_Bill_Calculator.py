e4f6fcb3fb4b1faa7d28a21274fd3c90a1071a8d
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-17 09:55:07 +0530

    Create Electricity_Bill_Calculator.py
    This program calculates the electricity bill amount based on the number of units consumed and the cost per unit. The user enters the units consumed and the rate per unit, and the program calculates the total bill amount.
    
    The bill amount is calculated using the formula:
    
    Bill Amount=Units Consumed×Rate Per Unit

---
+++ Electricity_Bill_Calculator.py
@@ -0,0 +1,6 @@
+units = float(input("Enter the number of units consumed: "))
+rate_per_unit = float(input("Enter the rate per unit: "))
+
+bill_amount = units * rate_per_unit
+
+print("Electricity Bill Amount =", bill_amount)
