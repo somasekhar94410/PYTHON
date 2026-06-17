792c05a042fc390d882e034026aaa45df1cd4c61
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-17 09:56:14 +0530

    Create Water_Bill_Calculator.py
    his program calculates the monthly water bill based on the amount of water consumed and the charge per unit of water. The user enters the water consumption and the rate per unit, and the program calculates the total water bill.
    
    The water bill is calculated using the formula:
    
    Water Bill=Water Consumption×Rate Per Unit

---
+++ Water_Bill_Calculator.py
@@ -0,0 +1,6 @@
+water_consumption = float(input("Enter water consumption (in units): "))
+rate_per_unit = float(input("Enter rate per unit: "))
+
+water_bill = water_consumption * rate_per_unit
+
+print("Monthly Water Bill =", water_bill)
