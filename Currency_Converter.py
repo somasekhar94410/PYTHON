cde4f133c2bbc7f8e9d5caa8845984536de7bc2b
somasekhar94410 <somasekhar94410@gmail.com>
2026-06-18 13:03:20 +0530

    Create  Currency_Converter.py
    This program converts an amount from one currency to another based on a user-provided exchange rate. The user enters the amount in the source currency and the exchange rate, and the program calculates the equivalent amount in the target currency.
    
    The currency conversion is calculated using the formula:
    
    Converted Amount=Amount×Exchange Rate

---
+++ Currency_Converter.py
@@ -0,0 +1,6 @@
+amount = float(input("Enter the amount: "))
+exchange_rate = float(input("Enter the exchange rate: "))
+
+converted_amount = amount * exchange_rate
+
+print("Converted Amount =", converted_amount)
