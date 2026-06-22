b87a91abd32e917ee1a3773bfd057f8b8b0190ff
Soma Sekhar <somasekhar94410@gmail.com>
2026-06-22 11:25:30 +0530

    Hotel Billing System

---
+++ unit 1/25.py
@@ -0,0 +1,7 @@
+room_charges = float(input("Enter Room Charges: "))
+food_charges = float(input("Enter Food Charges: "))
+service_charges = float(input("Enter Service Charges: "))
+
+total_bill = room_charges + food_charges + service_charges
+
+print("Total Hotel Bill =", total_bill)
